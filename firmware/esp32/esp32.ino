00275ae6bd9685affcb12a280f0aa67734643881
nextpassword99 <inderscop@gmail.com>
2025-09-19 22:17:49 -0500

    feat: Implementar websockets

--- firmware/esp32/esp32.ino
+++ firmware/esp32/esp32.ino
@@ -1,11 +1,13 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include <WebSocketsServer.h>
 #include <ArduinoJson.h>
 
 const char *ssid = "weeeeee";
 const char *password = "123";
 
 WebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
 
 const int PIN_SENSOR_HUMEDAD = 34;
 const int PIN_SENSOR_LUZ = 35;
@@ -29,16 +31,23 @@ bool bombaEncendida = false;
 bool modoManual = false;
 unsigned long tiempoUltimoRiego = 0;
 
+unsigned long ultimoEnvioWS = 0;
+const unsigned long intervaloWS = 2000;
+
 void configurarServidor();
 void manejarStatus();
 void manejarStart();
 void manejarStop();
 void manejarGetConfig();
 void manejarPostConfig();
+void manejarEventoWebSocket(uint8_t, WStype_t, uint8_t *, size_t);
+void enviarDatosSensorWebSocket();
 int obtenerUmbralHumedad();
 void manejarRiegoAutomatico();
-String tipoSueloToString(TipoSuelo tipo);
-TipoSuelo stringToTipoSuelo(const String &tipo);
+void encenderBomba();
+void apagarBomba();
+String tipoSueloToString(TipoSuelo);
+TipoSuelo stringToTipoSuelo(const String &);
 
 void setup()
 {
@@ -60,16 +69,27 @@ void setup()
     Serial.println("IP: " + WiFi.localIP().toString());
 
     configurarServidor();
+
+    webSocket.begin();
+    webSocket.onEvent(manejarEventoWebSocket);
+    Serial.println("🔌 WebSocket iniciado en puerto 81");
 }
 
 void loop()
 {
     server.handleClient();
+    webSocket.loop();
 
     if (!modoManual && millis() - tiempoUltimoRiego >= intervaloRiego)
     {
         tiempoUltimoRiego = millis();
         manejarRiegoAutomatico();
+    }
+
+    if (millis() - ultimoEnvioWS >= intervaloWS)
+    {
+        ultimoEnvioWS = millis();
+        enviarDatosSensorWebSocket();
     }
 }
 
@@ -82,6 +102,46 @@ void configurarServidor()
     server.on("/config", HTTP_POST, manejarPostConfig);
     server.begin();
     Serial.println("🌐 Servidor HTTP iniciado");
+}
+
+void manejarEventoWebSocket(uint8_t num, WStype_t tipo, uint8_t *payload, size_t length)
+{
+    switch (tipo)
+    {
+    case WStype_CONNECTED:
+        Serial.printf("🟢 WebSocket conectado [%u] desde %s\n", num, webSocket.remoteIP(num).toString().c_str());
+        break;
+
+    case WStype_DISCONNECTED:
+        Serial.printf("🔴 WebSocket desconectado [%u]\n", num);
+        break;
+
+    case WStype_TEXT:
+        Serial.printf("📨 Mensaje WebSocket [%u]: %s\n", num, payload);
+
+        break;
+
+    default:
+        break;
+    }
+}
+
+void enviarDatosSensorWebSocket()
+{
+    int humedad = analogRead(PIN_SENSOR_HUMEDAD);
+    int luz = analogRead(PIN_SENSOR_LUZ);
+
+    DynamicJsonDocument json(256);
+    json["humedad"] = humedad;
+    json["luz"] = luz;
+    json["nivel_luz"] = luz >= umbralLuz ? "claro" : "oscuro";
+    json["bomba"] = bombaEncendida;
+    json["modo"] = modoManual ? "manual" : "automatico";
+    json["suelo"] = tipoSueloToString(tipoSuelo);
+
+    String mensaje;
+    serializeJson(json, mensaje);
+    webSocket.broadcastTXT(mensaje);
 }
 
 void manejarRiegoAutomatico()
@@ -118,21 +178,6 @@ void apagarBomba()
     digitalWrite(PIN_PUMP_IN1, LOW);
     digitalWrite(PIN_PUMP_IN2, LOW);
     bombaEncendida = false;
-}
-
-int obtenerUmbralHumedad()
-{
-    switch (tipoSuelo)
-    {
-    case ARENOSO:
-        return 350;
-    case FRANCO:
-        return 500;
-    case ARCILLOSO:
-        return 650;
-    default:
-        return 500;
-    }
 }
 
 void manejarStatus()
@@ -197,23 +242,33 @@ void manejarPostConfig()
     }
 
     if (json.containsKey("tipoSuelo"))
-    {
         tipoSuelo = stringToTipoSuelo(json["tipoSuelo"]);
-    }
+
     if (json.containsKey("umbralLuz"))
-    {
         umbralLuz = json["umbralLuz"];
-    }
+
     if (json.containsKey("duracionRiego"))
-    {
         duracionRiego = json["duracionRiego"];
-    }
+
     if (json.containsKey("intervaloRiego"))
-    {
         intervaloRiego = json["intervaloRiego"];
-    }
 
     server.send(200, "application/json", "{\"mensaje\":\"Configuración actualizada\"}");
+}
+
+int obtenerUmbralHumedad()
+{
+    switch (tipoSuelo)
+    {
+    case ARENOSO:
+        return 350;
+    case FRANCO:
+        return 500;
+    case ARCILLOSO:
+        return 650;
+    default:
+        return 500;
+    }
 }
 
 String tipoSueloToString(TipoSuelo tipo)
